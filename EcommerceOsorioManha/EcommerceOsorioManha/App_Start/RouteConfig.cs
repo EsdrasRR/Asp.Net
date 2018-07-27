50572872d3638ac889b06f3eaac0e43f3cbaf8b2
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-07-27 08:35:32 -0300

    Criando ProdutoController

--- EcommerceOsorioManha/EcommerceOsorioManha/App_Start/RouteConfig.cs
+++ EcommerceOsorioManha/EcommerceOsorioManha/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EcommerceOsorioManha
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
