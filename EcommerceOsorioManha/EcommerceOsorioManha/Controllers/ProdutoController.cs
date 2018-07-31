76daa34721a65fb2960c761c8d0cc067400101a7
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-07-31 11:23:58 -0300

    Aula 31/07 Deletar e editar

--- EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
-﻿using EcommerceOsorioManha.Models;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,13 +8,14 @@ namespace EcommerceOsorioManha.Controllers
 {
     public class ProdutoController : Controller
     {
-        Context ctx = new Context();
+        Models.Context ctx = new Models.Context();
 
         // GET: Produto
         public ActionResult Index()
         {
             ViewBag.Data = DateTime.Now;
             ViewBag.Produtos = ctx.Produtos.ToList();
+            List<EcommerceOsorioManha.Models.Produto> produtos = ViewBag.Produtos;
             return View();
         }
         public ActionResult CadastrarProduto()
@@ -23,17 +25,42 @@ namespace EcommerceOsorioManha.Controllers
         [HttpPost]
         public ActionResult CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
         {
-            Produto produto = new Produto
+            Models.Produto produto = new Models.Produto
             {
                 Nome = txtNome,
-                Descricao = txtPreco,
+                Descricao = txtDescricao,
                 Preco = Convert.ToDouble(txtPreco),
-                Categoria = txtCategoria
+                Categoria = txtCategoria,
             };
 
             ctx.Produtos.Add(produto);
             ctx.SaveChanges();
 
+            return RedirectToAction("Index", "Produto");
+        }
+        public ActionResult RemoverProduto( int id)
+        {
+            ctx.Produtos.Remove(ctx.Produtos.Find(id));
+            ctx.SaveChanges();
+            return RedirectToAction("Index", "Produto"); 
+        }
+        public ActionResult AlterarProduto ( int id)
+        {
+            ViewBag.Produto = ctx.Produtos.Find(id);
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AlterarProduto (string txtNome, string txtDescricao, string txtPreco, string txtCategoria, int txtId)
+        {
+            Models.Produto produto = ctx.Produtos.Find(txtId);
+            produto.Nome = txtNome;
+            produto.Descricao = txtDescricao;
+            produto.Preco = Convert.ToDouble(txtPreco);
+            produto.Categoria = txtCategoria;
+
+            ctx.Entry(produto).State = EntityState.Modified;
+            ctx.SaveChanges();
             return RedirectToAction("Index", "Produto");
         }
     }
