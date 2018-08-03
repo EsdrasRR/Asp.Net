6821c5d520071f3b040c96ffb7aaac407c5306d8
Esdras Rafael <esdrasrocha1122@gmail.com>
2018-08-03 08:53:47 -0300

    Migrando comandos de BD da controller para a DAL

--- EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EcommerceOsorioManha.DAL;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -8,14 +9,12 @@ namespace EcommerceOsorioManha.Controllers
 {
     public class ProdutoController : Controller
     {
-        Models.Context ctx = new Models.Context();
 
         // GET: Produto
         public ActionResult Index()
         {
             ViewBag.Data = DateTime.Now;
-            ViewBag.Produtos = ctx.Produtos.ToList();
-            List<EcommerceOsorioManha.Models.Produto> produtos = ViewBag.Produtos;
+            ViewBag.Produtos = ProdutoDAO.RetornarProdutos();
             return View();
         }
         public ActionResult CadastrarProduto()
@@ -25,42 +24,27 @@ namespace EcommerceOsorioManha.Controllers
         [HttpPost]
         public ActionResult CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
         {
-            Models.Produto produto = new Models.Produto
-            {
-                Nome = txtNome,
-                Descricao = txtDescricao,
-                Preco = Convert.ToDouble(txtPreco),
-                Categoria = txtCategoria,
-            };
 
-            ctx.Produtos.Add(produto);
-            ctx.SaveChanges();
+            ProdutoDAO.CadastrarProduto(txtNome, txtDescricao, txtPreco, txtCategoria);
 
             return RedirectToAction("Index", "Produto");
         }
         public ActionResult RemoverProduto( int id)
         {
-            ctx.Produtos.Remove(ctx.Produtos.Find(id));
-            ctx.SaveChanges();
+            ProdutoDAO.RemoverProduto(id);
             return RedirectToAction("Index", "Produto"); 
         }
         public ActionResult AlterarProduto ( int id)
         {
-            ViewBag.Produto = ctx.Produtos.Find(id);
-
+            ViewBag.Produto = ProdutoDAO.BuscarProdutoPorId(id);
             return View();
         }
         [HttpPost]
         public ActionResult AlterarProduto (string txtNome, string txtDescricao, string txtPreco, string txtCategoria, int txtId)
         {
-            Models.Produto produto = ctx.Produtos.Find(txtId);
-            produto.Nome = txtNome;
-            produto.Descricao = txtDescricao;
-            produto.Preco = Convert.ToDouble(txtPreco);
-            produto.Categoria = txtCategoria;
 
-            ctx.Entry(produto).State = EntityState.Modified;
-            ctx.SaveChanges();
+            AlterarProduto(txtNome, txtDescricao, txtPreco, txtCategoria, txtId);
+            
             return RedirectToAction("Index", "Produto");
         }
     }
---
+++ EcommerceOsorioManha/EcommerceOsorioManha/DAL/ProdutoDAO.cs
@@ -0,0 +1,64 @@
+﻿using EcommerceOsorioManha.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EcommerceOsorioManha.DAL
+{
+    public class ProdutoDAO
+    {
+
+        private static Context ctx = new Context();
+
+        public static List<Produto> RetornarProdutos()
+        {
+            return ctx.Produtos.ToList();
+        }
+
+        public static void CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        {
+
+            Produto produto = new Produto
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = Convert.ToDouble(txtPreco),
+                Categoria = txtCategoria,
+            };
+
+            ctx.Produtos.Add(produto);
+            ctx.SaveChanges();
+
+        }
+
+        public static void RemoverProduto(int id)
+        {
+            ctx.Produtos.Remove(BuscarProdutoPorId(id));
+            ctx.SaveChanges();
+        }
+
+        public static void AlterarProduto(int id)
+        {
+           ctx.Produtos.Find(id);
+        }
+
+        public static void AlterarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria, int txtId)
+        {
+            Produto produto = BuscarProdutoPorId(txtId);
+            produto.Nome = txtNome;
+            produto.Descricao = txtDescricao;
+            produto.Preco = Convert.ToDouble(txtPreco);
+            produto.Categoria = txtCategoria;
+
+            ctx.Entry(produto).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public static Produto BuscarProdutoPorId(int id)
+        {
+                  return ctx.Produtos.Find(id);
+        }
+    }
+}
